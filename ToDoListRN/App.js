18c3fd9ecdf77f09bb17ecb8a54a20f7ff56db2e
Vini <126819626+vin1i@users.noreply.github.com>
2024-05-22 17:07:03 -0300

    .

--- ToDoListRN/App.js
+++ ToDoListRN/App.js
@@ -1,26 +1,54 @@
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button, Icon } from "react-native-elements";
 import UserForm from "./src/components/UserForm";
+import UserList from "./src/components/UserList";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
-export default (props) => {
+const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName="UserList">
-
+            <Stack.Navigator initialRouteName="UserList"
+                screenOptions={screenOptions}>
                 <Stack.Screen
                     name="UserList"
                     component={UserList}
+                    options={({ navigation }) => {
+                        return {
+                            title: "Lista de Usuários",
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => navigation.navigate('UserForm')}
+                                    type="clear"
+                                    icon={<Icon name="add" size={25} color="white" />}
+                                />
+                            )
+                        }
+                    }}
                 />
                 <Stack.Screen
-                    name="UserList"
+                    name="UserForm"
                     component={UserForm}
+                    options={{
+                        title: "Formulário de Usuários"
+                    }}
                 />
-
             </Stack.Navigator>
         </NavigationContainer>
     );
 };
+
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: "#8ec5fc"
+    },
+    headerTintColor: "#ffff",
+    headerTitleStyle: {
+        fontWeight: "bold"
+    }
+}
+
+export default App;
--- ToDoListRN/src/components/UserForm.js
+++ ToDoListRN/src/components/UserForm.js
@@ -2,5 +2,6 @@ import React from "react";
 import { Text } from "react-native";
 
 export default (props) => {
+    console.warn(Object.keys(props))
     return <Text>UserForm</Text>;
 };
